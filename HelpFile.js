08e611f05c4e36ffc2320b3e70571354c136fd01
Sabri Meeran <MrSherlockHolmes123@gmail.com>
2015-12-24 10:29:36 +0000

    See, I can be helpful ;P

---
+++ HelpFile.js
@@ -0,0 +1,16 @@
+{
+    "title": "", /* Add the name of your extension here */
+    "by": "", /* Scratch Username [Or Real Name] */
+    "about": "", /* Very Short Description */
+    "official": false, /* Don't set to true unless GrannyCookies wants you to. */
+    "blocks": [
+        ["b", "", ""] /* This one is a boolean based one :P */
+        ["w", "", ""] /* A block */
+        ["r", "", ""] /* A reporter, see previous extensions. The one on right is what to do when called */
+    ],
+    "menus": {}, /* If you want a nice looking menu */
+    "scripts": [], /* Scripts to load... beforehand? */
+    "onload": "", /* When it loads. */
+    "ondie": "", /* Hmm... */
+    "ontick": "" /* Ask GrannyCookies about the bottom two ;P*/
+}
