60c6598042aaf32f6a373491e1dc802ed92330f8
Sabri Meeran <MrSherlockHolmes123@gmail.com>
2015-12-24 10:35:46 +0000

    Updated

--- BlackListCheck.js
+++ BlackListCheck.js
@@ -5,7 +5,7 @@
     "official": false,
     "blocks": [
         ["b", "is %s safe?", "var string = arg1; string = string.replace(whitelist, ""); return !(string.match(blacklist));"]
-        ["w", "update lists", "var simpleAjaxGet = function(url, done) { var xml = new XMLHttpRequest(); xml.open(\"GET\", url); xml.send(); xml.onload = function() { done(xml); } }   simpleAjaxGet(\"https://raw.githubusercontent.com/MrSherlockHolmes/extraBlackList/master/blackList.txt\", function(xml) { var lines = xml.responseText.split(\'\\n\'); blacklist = lines[20]; whitelist = lines[24]; blacklist = new RegExp(blacklist.substring(1,blacklist.length-4), \"igm\"); whitelist = new RegExp(whitelist.substring(1,whitelist.length-4), \"igm\"); });"]
+        ["w", "update lists", "var simpleAjaxGet = function(url, done) { var xml = new XMLHttpRequest(); xml.open(\"GET\", url); xml.send(); xml.onload = function() { done(xml); } }   simpleAjaxGet(\"https://cdn.rawgit.com/MrSherlockHolmes/b3fc2b21b94040921c69/raw/107b4423ed633ab3353e7a11b466b1d58a46b732/extraBlackList.txt\", function(xml) { var lines = xml.responseText.split(\'\\n\'); blacklist = lines[20]; whitelist = lines[24]; blacklist = new RegExp(blacklist.substring(1,blacklist.length-4), \"igm\"); whitelist = new RegExp(whitelist.substring(1,whitelist.length-4), \"igm\"); });"]
     ],
     "menus": {},
     "scripts": [],
